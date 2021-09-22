9f709577d0e41514d186f2ca590ab8bbf08d101a
Hugh Parsons <hughparsonsnz@gmail.com>
2021-09-22 16:54:13 +1200

    added main.py and plotting rule in the readme

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ This repository stores all the relevant files fors our project.
 
 - Code must be commented --- focus on explaining why control flow structure and the functions exist.
 - Write simple unit tests in test.py to verify the functionality.
+- Keep plotting seperate from functions.
 
---
+++ main.py
@@ -0,0 +1,25 @@
+
+## module imports
+
+# data handling
+import numpy as np
+import pandas as pd
+
+# plotting
+from matplotlib import pyplot as plt
+# import seaborn as sns
+
+# type handling
+# from typing import List
+
+def main():
+    """
+    This function will handle calling the other functions as well as plotting.
+    """
+    pass
+
+if __name__ == "__main__":
+    main()
+    pass
+
+
