186db7435780c97a0e8b688a89825a45203841d6
Hugh Parsons <hughparsonsnz@gmail.com>
2021-09-22 16:30:42 +1200

    updated readme for intial rules, ignoring pdfs, data and test file added.

---
+++ test.py
@@ -0,0 +1,15 @@
+"""
+This file shoud contain tests.
+
+try to format them as:
+    test(FunctionBeingCalled)():
+
+then add the tests to the main function.
+"""
+
+def main()->None:
+    pass
+
+
+if __name__ == "__main__":
+    pass
