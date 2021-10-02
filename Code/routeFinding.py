f5d3054e71454a78cbf688014b8591d75c992c57
Hugh Parsons <hughparsonsnz@gmail.com>
2021-10-02 16:02:25 +1300

    tested subgraph finder on south auckland

--- Code/routeFinding.py
+++ Code/routeFinding.py
@@ -10,11 +10,11 @@ from random import randint, seed
 @dataclass
 class RouteFinder:
     """Will change just keeping the work localised for now"""
-    nodes: Dict[int, int] # node: demand - might change this later 
+    nodes: Dict[str, int] # node: demand - might change this later 
     adjacencyMatrix: List[List[int]] # durations between nodes: [i][j] = duration i to j
     maxDemand: int = 26
 
-    def generate(self, numbers: List[int], k: int):
+    def generate(self, numbers: List[str], k: int):
         """
         Generates combinations of numbers C k, preserves order.
         """
@@ -37,14 +37,14 @@ class RouteFinder:
         Performs recursive calls incrementing maxNodes until no viable subgraph
         exists.
         """
-        ans = [subGraph for subGraph in self.generate(list(self.nodes.keys()), maxNodes) if sum(nodes[node] for node in subGraph) <= 26]
+        ans = [subGraph for subGraph in self.generate(list(self.nodes.keys()), maxNodes) if sum(self.nodes[node] for node in subGraph) <= 26]
         if not ans: # checking whether any subGraphs passed the test
             return []
         return ans + self.findValidSubgraphs(maxNodes+1)
 
        
 
-    def antColonyTSP(self, subGraph: List[int]):
+    def antColonyTSP(self, subGraph: List[int], iterationThreshold: int, numAnts: int, numNodes: int):
         """
         Function to find the best circuit for some subgraph using the cheapest insertion method.
 
@@ -58,20 +58,50 @@ class RouteFinder:
                 a = pheremone intensity regulation parameter
                 n = visibility of node j from i = 1/d where d is the distance between two nodes
                 b = visibility strength parameter
+
+        psuedocode:
+            --Taken from: https://doi.org/10.1109/4235.585892
+            
+            Iteration loop:
+                each ant is positioned at a starting node
+                step loop:
+                    each ant applies a state transition rule to incrementally build a solution. and a local pheremone 
+                    updating rule
+                until: all ants have a complete solution
+                Global pheremone rule applied
+            until: iteration limit
+
+        rules:
+            transition:
+                Transition from r->s given by:
+                s = Max(t(r,u)*(n(r,u)**b) for u in unvisitedCities) if q <= q_0 else random
+                where:
+                    q = random var on [0,1]
+                    q_0 is a parameter
+            local:
+
+            global:
+                t(r,s) = (1-a)*t(r,s) + sum_{k=1}^{m}(deltaT_{k}(r,s))
+                where:
+                    deltaT_{k}(r,s)= 1/L_k if (r,s) in ant_k.tour else 0
+                    L_k = total tour length for ant k
         """
         pass
 
 
 
-
+from glob import glob
 if __name__ == "__main__":
     seed(508) # keep the tests the same
 
-    nodes = {i: randint(4, 15) for i in range(10)}
+    
+
+    # nodes = {i: randint(4, 15) for i in range(10)}
     # print(nodes)
 
-    routeFinder = RouteFinder(nodes=nodes, adjacencyMatrix=[])
-    subgraphs = routeFinder.findValidSubgraphs(1)
+    # routeFinder = RouteFinder(nodes=nodes, adjacencyMatrix=[])
+    # subgraphs = routeFinder.findValidSubgraphs(1)
+    # print(subgraphs)
     # for g in subgraphs:
     #     print(f"nodes:{g}\t\t total demand:{sum(nodes[n] for n in g)}")
     pass
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 
 ## module imports
+from Code import routeFinding
 
 # data handling
 import numpy as np
 import pandas as pd
+from glob import glob
+# from collections import Counter
 
 # plotting
 from matplotlib import pyplot as plt
@@ -12,6 +15,20 @@ from matplotlib import pyplot as plt
 # type handling
 # from typing import List
 
+
+def prims(graph, adjacencyMatrix, root):
+    """incomplete"""
+    for node in graph:
+        node.distance = float('inf')
+        node.predecessor = None
+    root.distance = 0
+    q = [node for node in graph]
+    while q:
+        u = min(q, key = lambda x: x.distance())
+        for node in u.adjacents:
+            if node in q and adjacencyMatrix[u][node] < node.distance:
+                pass
+
 def main():
     """
     This function will handle calling the other functions as well as plotting.
@@ -19,7 +36,28 @@ def main():
     pass
 
 if __name__ == "__main__":
-    main()
+    demands = glob("./Data/AverageDemands.csv")[0]
+
+    runSouth = True
+    if runSouth:
+        south = ['Countdown Manukau', 'Countdown Manukau Mall',	'Countdown Manurewa', 'Countdown Papakura',	'Countdown Roselands', 'Countdown Takanini', 'SuperValue Flatbush', 'SuperValue Papakura']
+        data = pd.read_csv(demands, delimiter=',',index_col=0)
+
+        nodes = {store: data['Monday'][store] for store in south}
+
+        # print(nodes)
+        temp = routeFinding.RouteFinder(nodes=nodes, adjacencyMatrix=[])
+        validRoutes = temp.findValidSubgraphs()
+        
+        validRoutesByLength = {i:[] for i in range(1,len(max(validRoutes, key = len))+1)}
+        for route in validRoutes:
+            validRoutesByLength[len(route)].append(route)
+
+        # for i in validRoutesByLength:
+            # print(f"i:{i}, routes:{validRoutesByLength[i]}")
+        # print(len(validRoutes))
+        pass
+    # main()
     pass
 
 
