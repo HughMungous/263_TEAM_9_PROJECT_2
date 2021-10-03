ac0d539fecbe85ef34a89cd35d8215fa25b0a6a2
Hugh Parsons <hughparsonsnz@gmail.com>
2021-10-03 22:52:36 +1300

    added function to generate routes for a given day + region

--- main.py
+++ main.py
@@ -15,19 +15,36 @@ from matplotlib import pyplot as plt
 # type handling
 # from typing import List
 
+# global variables: dont edit please 🙏
+depot = "Distribution Centre Auckland"
+locations = dataInput.readLocationGroups()
+stores, demands = dataInput.readAverageDemands()
+travelDurations = dataInput.readTravelDurations()
+coordinates = dataInput.readStoreCoordinates()
 
-def prims(graph, adjacencyMatrix, root):
-    """incomplete"""
-    for node in graph:
-        node.distance = float('inf')
-        node.predecessor = None
-    root.distance = 0
-    q = [node for node in graph]
-    while q:
-        u = min(q, key = lambda x: x.distance())
-        for node in u.adjacents:
-            if node in q and adjacencyMatrix[u][node] < node.distance:
-                pass
+routeFinder = routing.Pathfinder(travelDurations)
+
+def generateRoutes(day: str, region: str, nPartitions:int=5):
+    """generates the TSP routes/partitions for a given day and region """
+    # getting the specific demands for the day and region
+    regionalDemands = {location: demands[day][location] for location in locations['region']}
+        
+    regionRoutingObj = routing.Region(nodes=regionalDemands, locations=coordinates)
+
+    validSubgraphs = regionRoutingObj.findValidSubgraphs()
+    partitions = regionRoutingObj.createPartitions(validSubgraphs, nPartitions)
+    
+    routes = []
+    for partition in partitions:
+        temp = []
+        for subgraph in partition:
+            solution = routeFinder.nearestNeighbour([depot] + subgraph)
+            # rearranging so the depot is always the start
+            temp.append(solution[solution.index(depot):] + solution[:solution.index(depot)])
+        routes.append(temp)
+
+    return routes
+    
 
 def main():
     """
@@ -36,7 +53,18 @@ def main():
     pass
 
 if __name__ == "__main__":
-    
+    routes = generateRoutes('Monday', 'North')
+
+    for route in routes:
+        avg = 0
+        for partition in route:
+            totalTime = 0
+            for i in range(len(partition)-1):
+                totalTime += travelDurations[partition[i]][partition[i+1]] + 0.125*southDemands['Monday'][partition[i+1]]
+                totalTime += travelDurations[partition[-1]][depot]
+            # print(f"route:{partition},\t\t totalTime:{totalTime:.3f}")
+            avg += totalTime
+        print(f"Partition: {route}\nAverage duration for the partition: {avg/len(route):.3f}\nNum trucks: {len(route)}")
     # main()
     pass
 
