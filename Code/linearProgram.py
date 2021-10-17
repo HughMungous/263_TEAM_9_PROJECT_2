18c8d78e7b6a175ff04af5268be6a33de26d19c8
Hugh Parsons <hughparsonsnz@gmail.com>
2021-10-18 10:09:58 +1300

    added total time constraint since the average time constraint would be non-lineaer

--- Code/linearProgram.py
+++ Code/linearProgram.py
@@ -11,54 +11,62 @@ from typing import List
 
 
 def findBestPartition(day: str, region: str, routes: List[List[str]], stores: List[str], durations: List[float], maxTrucks: int = 60, disp: bool = False):
-'''  
-Parameters
----------
-day: str
-     Day of the week
+    '''  
+        Parameters
+        ---------
+            day: str
+                Day of the week
 
-region: str
-        Region in Auckland of North, Central, West or South
-        
-routes: list of a list of strings
-        Multiple routes, each route is a list of strings
+            region: str
+                    Region in Auckland of North, Central, West or South
+                    
+            routes: list of a list of strings
+                    Multiple routes, each route is a list of strings
 
-stores: list of stings
-        Woolworth NZ operated supermarket stores in Auckland
-        
-durations: list of floats
-            Travel durations in seconds between each pair of stores and distribution points
-           
-maxTrucks: int
-           Number of trucks available which is 60
+            stores: list of stings
+                    Woolworth NZ operated supermarket stores in Auckland
+                    
+            durations: list of floats
+                        Travel durations in seconds between each pair of stores and distribution points
+                    
+            maxTrucks: int
+                    Number of trucks available which is 60
 
-Returns
--------
-routesChosen: np.array
-              Array of lowest-cost routes
+        Returns
+        -------
+            routesChosen: np.array
+                        Array of lowest-cost routes
 
-Notes
------
-This linear program uses the PuLP package
+        Notes
+        -----
+            This linear program uses the PuLP package
 
-'''
+    '''
 
     # Variable of cost
     cost = lambda x: 225*x + 50 * max(0,x-4)
         
     # variable for whether a route is chosen 
     possibleRoutes = [LpVariable(region+f"_route_{i}", 0, 1, LpInteger) for i in range(len(routes))]
+
+    slackTime = LpVariable("slack_time", 0, cat=LpContinuous)
     
     # The variable 'routing_model' to contain the problem data where the objective is to minimise
     routing_model = pulp.LpProblem(f"{day}_{region}_RoutingModel", LpMinimize)
 
     # Objective Function is added to 'routing_model'
-    routing_model += pulp.lpSum([cost(durations[i]) * possibleRoutes[i] for i in range(len(routes))])
+    routing_model += (pulp.lpSum([cost(durations[i]) * possibleRoutes[i] for i in range(len(routes))]) + slackTime*10000)
 
     # specify the maximum number of trucks
     routing_model += (
         pulp.lpSum(possibleRoutes) <= maxTrucks,
         "Maximum_number_of_trucks",
+    )
+
+    # Cant specify an average duration constraint since the denominator will change, this assumes 6 ish routes per region
+    routing_model += (
+        pulp.lpSum([durations[i] * possibleRoutes[i] for i in range(len(routes))]) - slackTime <= 25,
+        "Average_duration_constraint",
     )
 
     # A store must be only visited once
@@ -69,6 +77,7 @@ This linear program uses the PuLP package
             f"Must_supply_{store}",
         )
 
+    # routing_model.writeLP(f"LPFiles/{day}{region}.lp")
     # The routing_model is solved using PULP_CBC_CMD
     routing_model.solve(PULP_CBC_CMD(msg=disp))
 
