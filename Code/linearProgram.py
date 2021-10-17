6c21fca682a46fda91ab4ec9b949b98bfaf0538e
emilyhp <80794341+emilyhp@users.noreply.github.com>
2021-10-17 19:55:11 +1300

    Docstrings and comments added

--- Code/linearProgram.py
+++ Code/linearProgram.py
@@ -1,6 +1,24 @@
+'''
+Mixed-integer linear pogram which solves for the most optimal solution
+
+Parameters
+---------
+
+
+Returns
+-------
+routesChosen: np.array
+              Array of lowest-cost routes
+
+Notes
+-----
+This linear program uses the PuLP package in Python
+
+'''
+
 
 import numpy as np
-import pandas as pd # We will discuss this more next week!
+import pandas as pd
 from pulp import *
 
 from typing import List
@@ -13,9 +31,10 @@ def findBestPartition(day: str, region: str, routes: List[List[str]], stores: Li
     # variable for whether a route is chosen 
     possibleRoutes = [LpVariable(region+f"_route_{i}", 0, 1, LpInteger) for i in range(len(routes))]
     
+    # The variable 'routing_model' to contain the problem data where the objective is to minimise
     routing_model = pulp.LpProblem(f"{day}_{region}_RoutingModel", LpMinimize)
 
-    # Objective Function
+    # Objective Function is added to 'routing_model'
     routing_model += pulp.lpSum([cost(durations[i]) * possibleRoutes[i] for i in range(len(routes))])
 
     # specify the maximum number of tables
@@ -32,13 +51,17 @@ def findBestPartition(day: str, region: str, routes: List[List[str]], stores: Li
             f"Must_supply_{store}",
         )
 
+    # The routing_model is solved using PULP_CBC_CMD
     routing_model.solve(PULP_CBC_CMD(msg=disp))
 
+    # Initialising the array routesChosen
     routesChosen = []
     
+    # Iterating through the routes
     for i in range(len(routes)):
         if possibleRoutes[i].value() == 1.0:
             
             routesChosen.append(routes[i])
-
+            
+    # The routes chosen and status of the solution is returned
     return routesChosen, LpStatus[routing_model.status] == "Optimal"
